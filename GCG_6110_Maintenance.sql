0670e0a4d31243c2c1ab3259c843de065f139211
Service Web <sw@gssmail.com>
2024-03-15 11:41:51 -0500

    Commit changes in C:\Websites\Git\emarlow\6110\GCG_6110_Maintenance.sql

---
+++ GCG_6110_Maintenance.sql
@@ -0,0 +1,9 @@
+-- Database: GLOBALPLA Metadata version: 2 Exported: Oct 22, 2020
+-- Begin Tables
+SET LEGACYTYPESALLOWED = ON;
+-- Begin Multi-Line Statement
+CREATE TABLE "GCG_6110_SETTINGS" (
+ "SETTINGS" CHAR(50) );
+-- End Multi-Line Statement
+SET LEGACYTYPESALLOWED = OFF;
+-- End Tables
